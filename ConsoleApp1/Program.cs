ed275d44c2e1b6b84d2e86943e7f96db66141a70
Ahmet Furkan Bozkurt <burislifurik@hotmail.com>
2024-05-29 13:53:07 +0300

    Gerekesiz Kütüphaneler çıkarıldı

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,5 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
+﻿using System;
 using System.Text.RegularExpressions;
-using System.Text;
-using System;
-
 class Solution
 {
 
